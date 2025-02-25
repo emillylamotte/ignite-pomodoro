8ee66d60fca3f3ebed77af30537c26f89a0bf7c7
emilly.lamotte <emilly.lamotte@furg.br>
2025-02-25 20:16:55 -0300

    Trying using hash router

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import { themeDefault } from './themes/default.ts'
 import { GlobalStyles } from './themes/globalStyle.ts'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Router } from './routers/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={themeDefault}>
-      <BrowserRouter basename="/ignite-pomodoro">
+      <HashRouter>
         <Router />
-      </BrowserRouter>
+      </HashRouter>
       <GlobalStyles />
     </ThemeProvider>
   </StrictMode>,
