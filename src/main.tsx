55d1ef87ef89725809af34b52ccafe31a24222fe
emilly.lamotte <emilly.lamotte@furg.br>
2025-02-25 20:26:53 -0300

    Returning to browser router

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import { themeDefault } from './themes/default.ts'
 import { GlobalStyles } from './themes/globalStyle.ts'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Router } from './routers/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={themeDefault}>
-      <HashRouter>
+      <BrowserRouter basename="/ignite-pomodoro/">
         <Router />
-      </HashRouter>
+      </BrowserRouter>
       <GlobalStyles />
     </ThemeProvider>
   </StrictMode>,
