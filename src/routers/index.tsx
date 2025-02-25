3b6154b7b9c7acce23c1960bb4d3137d36847a73
emilly.lamotte <emilly.lamotte@furg.br>
2025-02-25 19:55:33 -0300

    Config outlet routes

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
+import { History } from '../pages/History'
+import { Layout } from '../pages/Layout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/ignite-pomodoro" element={<Home />} />
-      <Route path="*" element={<Home />} />
+      <Route path="/ignite-pomodoro" element={<Layout />}>
+        <Route path="/ignite-pomodoro" element={<Home />} />
+        <Route path="/ignite-pomodoro/home" element={<Home />} />
+        <Route path="/ignite-pomodoro/history" element={<History />} />
+        <Route path="*" element={<Home />} />
+      </Route>
     </Routes>
   )
 }
