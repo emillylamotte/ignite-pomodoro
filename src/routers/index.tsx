f4771e25d28390600867686cb39927d086a39904
emilly.lamotte <emilly.lamotte@furg.br>
2025-02-26 16:16:30 -0300

    Include outlet again

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { History } from '../pages/History'
+import { Layout } from '../pages/Layout'
 
 export function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/history" element={<History />} />
-      <Route path="*" element={<Home />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+        <Route path="*" element={<Home />} />
+      </Route>
     </Routes>
   )
 }
