406570387a5cfb6d7a520d736c3245ee8eb84d7d
emilly.lamotte <emilly.lamotte@furg.br>
2025-02-26 16:12:29 -0300

    Fix hash router

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,17 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { History } from '../pages/History'
-import { Layout } from '../pages/Layout'
 
-export function Router() {
+export function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />{' '}
-        <Route path="home" element={<Home />} />
-        <Route path="history" element={<History />} />
-        <Route path="*" element={<Home />} />
-      </Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/history" element={<History />} />
+      <Route path="*" element={<Home />} />
     </Routes>
   )
 }
